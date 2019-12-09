5234380d00d5fb07cf722d464b77b01cbd3cefd4
Sven Ullmann <kontakt@sumedia-webdesign.de>
2019-12-09 01:09:10 +0100

    change naming

--- sumedia-amapn.php
+++ sumedia-amapn.php
@@ -8,7 +8,7 @@
  * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3 or higher
  *
  * @wordpress-plugin
- * Plugin Name: Sumedia Amazon Partnernet
+ * Plugin Name: Sumedia AmaPn
  * Plugin URI:  /
  * Description: Use Amazon Partnernet Widgets with non-tracking data privacy
  * Version:     0.1.1
